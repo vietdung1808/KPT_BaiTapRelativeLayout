1113c3695d1fb2e0d726bc0840031f670368604e
vietdung1808 <vietdung1808@gmail.com>
2020-06-15 11:52:49 +0700

    Bai 1

---
+++ app/src/main/java/com/vietdung/kpt_baitaprelativelayout/MainActivity.java
@@ -0,0 +1,14 @@
+package com.vietdung.kpt_baitaprelativelayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
